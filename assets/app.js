21d87033f76e30bd9f13badd5dafe3d1beaf903f
Jameson Campbell <jcampbell@flexterapp.com>
2019-04-15 13:56:54 -0500

    train, destination, and frequency display on client-side

--- assets/app.js
+++ assets/app.js
@@ -1,17 +1,16 @@
   // Initialize Firebase
   var config = {
-    apiKey: "AIzaSyBF-tm1raCZ04vUD3KKHZyHsyMM5rWvrAg",
-    authDomain: "train-scheduler-ba389.firebaseapp.com",
-    databaseURL: "https://train-scheduler-ba389.firebaseio.com",
-    projectId: "train-scheduler-ba389",
-    storageBucket: "train-scheduler-ba389.appspot.com",
-    messagingSenderId: "536871174028"
+      apiKey: "AIzaSyBF-tm1raCZ04vUD3KKHZyHsyMM5rWvrAg",
+      authDomain: "train-scheduler-ba389.firebaseapp.com",
+      databaseURL: "https://train-scheduler-ba389.firebaseio.com",
+      projectId: "train-scheduler-ba389",
+      storageBucket: "train-scheduler-ba389.appspot.com",
+      messagingSenderId: "536871174028"
   };
   firebase.initializeApp(config);
-
   var database = firebase.database();
 
-  $(`#add-train-btn`).click(function(event) {
+  $(`#add-train-btn`).click(function (event) {
       event.preventDefault();
 
       var train = $(`#train-name-input`).val().trim();
@@ -20,22 +19,51 @@
       var trainFrequency = $(`#train-frequency`).val().trim();
 
       var newTrain = {
-          train: train, 
-          destination: destination, 
-          firstTrainTime: firstTrainTime, 
+          train: train,
+          destination: destination,
+          firstTrainTime: firstTrainTime,
           trainFrequency: trainFrequency
       };
 
-  database.ref().push(newTrain);
+      database.ref().push(newTrain);
 
-  console.log(newTrain.train);
-  console.log(newTrain.destination);
-  console.log(newTrain.firstTrainTime);
-  console.log(newTrain.trainFrequency);
+      console.log(newTrain.train);
+      console.log(newTrain.destination);
+      console.log(newTrain.firstTrainTime);
+      console.log(newTrain.trainFrequency);
 
-  train = $(`#train-name-input`).val().trim("");
-  destination = $(`#destination-input`).val().trim("");
-  firstTrainTime = $(`#first-train-time-input`).val().trim("");
-  trainFrequency = $(`#train-frequency`).val().trim("");
-});
+      train = $(`#train-name-input`).val("");
+      destination = $(`#destination-input`).val("");
+      firstTrainTime = $(`#first-train-time-input`).val("");
+      trainFrequency = $(`#train-frequency`).val("");
+  });
 
+  database.ref().on("child_added", function (childSnapshot) {
+
+      var train = childSnapshot.val().train;
+      var destination = childSnapshot.val().destination;
+      var trainFrequency = childSnapshot.val().trainFrequency;
+      var firstTrainTime = childSnapshot.val().firstTrainTime;
+
+      var newTr = {
+        train: train,
+        destination: destination,
+        trainFrequency: trainFrequency,
+        firstTrainTime: firstTrainTime
+    };
+
+      console.log(childSnapshot.val());
+      console.log(trainFrequency);
+      $("tbody").append(makeRow(newTr));
+
+  })
+
+  function makeRow(data) {
+      return `
+        <tr>
+            <td>${data.train}</td>
+            <td>${data.destination}</td>
+            <td>${parseInt(data.trainFrequency)}</td>
+        </tr>
+      `
+  }
